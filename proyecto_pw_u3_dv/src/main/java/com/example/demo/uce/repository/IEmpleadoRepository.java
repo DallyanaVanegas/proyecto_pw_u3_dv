71a6eb19e4ac6cc31bafc546c1f19a1c3842b003
Dallyana Vanegas <85204662+DallyanaVanegas@users.noreply.github.com>
2022-08-17 19:43:20 -0500

    Add files via upload

---
+++ proyecto_pw_u3_dv/src/main/java/com/example/demo/uce/repository/IEmpleadoRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.uce.repository;
+
+import com.example.demo.uce.repository.modelo.Empleado;
+
+public interface IEmpleadoRepository {
+	
+	public void crear(Empleado empleado);
+	public void actualiza(Empleado empleado);
+	public void eliminar(Integer id);
+	public Empleado buscar (Integer id);
+	
+}
---
+++ proyecto_pw_u3_dv/src/main/java/com/example/demo/uce/service/IEmpleadoService.java
@@ -0,0 +1,13 @@
+package com.example.demo.uce.service;
+
+import com.example.demo.uce.repository.modelo.Empleado;
+
+public interface IEmpleadoService {
+	
+	public void crear(Empleado empleado);
+	public void actualizar(Empleado empleado);
+	public Empleado buscarPorId(Integer id);
+	public void eliminar(Integer id);
+	
+
+}
