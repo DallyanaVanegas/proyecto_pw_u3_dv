b1b79e957eba17e077cd03097d82a452ccb3bf03
Dallyana Vanegas <85204662+DallyanaVanegas@users.noreply.github.com>
2022-08-22 01:07:57 -0500

    tarea16
    RestFul Estudiante

---
+++ proyecto_pw_u3_dv/src/main/java/com/example/demo/uce/repository/modelo/Estudiante.java
@@ -0,0 +1,69 @@
+package com.example.demo.uce.repository.modelo;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Estudiante")
+public class Estudiante {
+	@Id
+	@Column(name="estu_id")
+	private Integer id;
+	@Column(name="estu_nombre")
+	private String nombre;
+	@Column(name="estu_apellido")
+	private String apellido;
+	@Column(name="estu_fecha_nacimiento")
+	private LocalDateTime fechaNacimiento;
+	@Column(name="estu_semestre")
+	private Integer semestre;
+	@Column(name="estu_carrera")
+	private String carrera;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+	public LocalDateTime getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(LocalDateTime fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public Integer getSemestre() {
+		return semestre;
+	}
+	public void setSemestre(Integer semestre) {
+		this.semestre = semestre;
+	}
+	public String getCarrera() {
+		return carrera;
+	}
+	public void setCarrera(String carrera) {
+		this.carrera = carrera;
+	}
+	
+	
+	
+	
+	
+	
+}
